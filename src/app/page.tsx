fb0ac0c89b8e82d414df6e8b48965c773068e27f
Fischer <fischerrobson@gmail.com>
2023-05-17 10:43:59 -0300

    landing page finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { User } from 'lucide-react'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -17,6 +19,36 @@ export default function Home() {
             memórias!
           </p>
         </a>
+
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="nlw spacetime logo" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className=" text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-xl leading-tight ">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <a className="inline-block cursor-pointer rounded-full bg-green-500 px-5 py-4 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600">
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+            href="https://rocketseat.com.br"
+          >
+            Rocketseat
+          </a>
+        </div>
       </div>
 
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
